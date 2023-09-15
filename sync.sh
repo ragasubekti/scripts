1e4841513ab553c98c7ae9b7b9beb5a6153f3bee
Raga Subekti <ragasubekti@gmail.com>
2023-09-16 05:50:09 +0900

    adbsync: just sync both storage

--- sync.sh
+++ sync.sh
@@ -1,34 +1,29 @@
 #!/usr/bin/env bash
 
-SDCARD=0
-
-[[ $2 ]] && SDCARD=1
 ! [[ $1 ]] && echo "Specify the device bruh" && exit 1
 
-if ! [[ $SDCARD ]]; then
+SDCARD_PATH=$(adb -s "$1" shell ls /storage | sed '1p;d')
+
+adbsync \
+    --adb-option "s" "$1" \
+    --show-progress \
+    --exclude "*.mp4" \
+    --exclude "*.wav" \
+    --exclude "*.mp3" \
+    --exclude "*.flac" \
+    --exclude "*.webm" \
+    --exclude "Android/*" \
+    pull \
+    "/storage/emulated/0/" \
+    "/backup/android/sdcard"
+
+if ! [[ $SDCARD_PATH = "emulated" ]] && ! [[ $SDCARD_PATH = "self" ]]; then
     adbsync \
         --adb-option "s" "$1" \
         --show-progress \
-        --exclude "*.mp4" \
-        --exclude "*.wav" \
-        --exclude "*.mp3" \
-        --exclude "*.flac" \
-        --exclude "*.webm" \
         --exclude "Android/*" \
+        --exclude "Audiobooks/*" \
         pull \
-        "/storage/emulated/0/" \
-        "/backup/android/sdcard"
-else
-    SDCARD_PATH=$(adb -s "$1" shell ls /storage | sed '1p;d')
-
-    if ! [[ $SDCARD_PATH = "emulated" ]] && ! [[ $SDCARD_PATH = "self" ]]; then
-        adbsync \
-            --adb-option "s" "$1" \
-            --show-progress \
-            --exclude "Android/*" \
-            --exclude "Audiobooks/*" \
-            pull \
-            "/storage/${SDCARD_PATH}/" \
-            "/backup/android/external_sd"
-    fi
+        "/storage/${SDCARD_PATH}/" \
+        "/backup/android/external_sd"
 fi
