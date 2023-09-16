29e566b94124525c48bb0eb36879c9d14f840fdd
Raga Subekti <ragasubekti@gmail.com>
2023-09-16 14:10:26 +0900

    pepega

--- compress_btrfs.sh
+++ compress_btrfs.sh
@@ -3,5 +3,5 @@
 VOLUME=("/data" "/books" "/data/calibre" "/backup")
 
 for EVOL in "${VOLUME[@]}"; do
-    btrfs filesystem defrag -czsf -r "${EVOL}"
+    btrfs filesystem defrag -czstd -r "${EVOL}"
 done
