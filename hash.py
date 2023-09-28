132022c0736e7240b32018a6c58e8d6241c6834d
Raga Subekti <ragasubekti@gmail.com>
2023-09-28 23:43:12 +0900

    py: add mtime to check for dup

--- hash.py
+++ hash.py
@@ -1,6 +1,8 @@
+from ast import mod
 import hashlib
 import sqlite3
 import os
+import tqdm
 
 
 def check_hash(file_path):
@@ -11,13 +13,23 @@ def check_hash(file_path):
     return md5.hexdigest()
 
 
-def store_hash_in_db(db_path, file_path, hash):
+def store_hash_in_db(db_path, file_path, hash, mtime):
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
-    c.execute("INSERT INTO files (file_path, hash) VALUES (?, ?)",
-              (file_path, hash))
+    c.execute("INSERT INTO files (file_path, hash, modified_time) VALUES (?, ?, ?)",
+              (file_path, hash, mtime))
     conn.commit()
     conn.close()
+
+
+def check_file_mtime(db_path, file_path, mtime):
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute("SELECT hash FROM files WHERE file_path = ? AND modified_time = ?",
+              (file_path, mtime))
+    hash = c.fetchone()
+    conn.close()
+    return hash
 
 
 def check_hash_in_db(db_path, hash):
@@ -29,37 +41,71 @@ def check_hash_in_db(db_path, hash):
     return file_path
 
 
+def create_hard_link(file_path, new_file_path):
+    if os.path.exists(new_file_path):
+        print("\033[41m[REMOVING]\033[0m", end="")
+        print(" {}".format(new_file_path))
+        # os.remove(new_file_path)
+
+    print("\033[42m[HARD_LINK]\033[0m", end="")
+    print(" {} > {}".format(new_file_path, file_path))
+
+    # os.link(file_path, new_file_path)
+
+
 def delete_file(file_path):
     print("Delete" + file_path)
     # os.remove(file_path)
 
 
+def get_total_items_in_folder(dir_path):
+    total_items = 0
+    for root, dirs, files in os.walk(dir_path):
+        total_items += len(files) + len(dirs)
+    return total_items
+
+
 def main():
     db_path = "hashes.db"
-    dir_path = "/"
+    dir_path = "/archive/"
+
+    total_items = get_total_items_in_folder(dir_path)
+    progress_bar = tqdm.tqdm(total=total_items)
 
     if not os.path.exists(db_path):
         conn = sqlite3.connect(db_path)
         c = conn.cursor()
-        c.execute("CREATE TABLE files (file_path TEXT, hash TEXT)")
+        c.execute(
+            "CREATE TABLE files (file_path TEXT, hash TEXT, modified_time INTEGER)")
         conn.commit()
         conn.close()
 
-    for file_path in os.listdir(dir_path):
-        print("Checking: " + file_path)
-        hash = check_hash(os.path.join(dir_path, file_path))
+    for root, dirs, files in os.walk(dir_path):
+        for file_path in files:
+            file_path = os.path.join(root, file_path)
 
-        path_in_db = check_hash_in_db(db_path, hash)
+            modified_time = os.path.getmtime(file_path)
 
-        if path_in_db is not None:
-            print([path_in_db[0], file_path, path_in_db[0] == file_path])
+            hash_file_mtime = check_file_mtime(
+                db_path, file_path, modified_time)
 
-            if path_in_db[0] != file_path:
-                # original_file_path = check_hash_in_db(db_path, hash)
-                delete_file(os.path.join(dir_path, file_path))
+            if hash_file_mtime:
+                progress_bar.update()
+                continue
 
-        else:
-            store_hash_in_db(db_path, file_path, hash)
+            hash = check_hash(file_path)
+
+            path_in_db = check_hash_in_db(db_path, hash)
+            progress_bar.update()
+
+            if path_in_db is not None:
+                if path_in_db[0] != file_path:
+                    create_hard_link(file_path, path_in_db[0])
+
+            else:
+                store_hash_in_db(db_path, file_path, hash, modified_time)
+
+    progress_bar.close()
 
 
 if __name__ == "__main__":
