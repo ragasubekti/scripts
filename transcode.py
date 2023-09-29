1ea03e0c30ce82486de8d0da87e1059218c69e4f
Raga Subekti <ragasubekti@gmail.com>
2023-09-29 20:38:49 +0900

    py: why do i WRITE this in python

--- transcode.py
+++ transcode.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import sqlite3
+from colorama import Fore, Back, Style
 
 
 def connect_to_db():
@@ -10,7 +11,6 @@ def connect_to_db():
 
 def create_table_if_not_exists(conn):
     c = conn.cursor()
-
     c.execute(
         "CREATE TABLE IF NOT EXISTS transcoded_videos (source_file TEXT, target_file TEXT)")
 
@@ -36,66 +36,112 @@ def check_if_transcoded(conn, video_file):
     return result is not None
 
 
+def check_video(video_file):
+    check = ["ffprobe", "-v", "error", "-i", video_file]
+    returncode = subprocess.run(check).returncode
+
+    return returncode == 0
+
+
 def get_bitrate(video_file):
     command = ["ffprobe", "-hide_banner", "-i", video_file, "-show_format"]
-    output = subprocess.check_output(command)
+    dev_null = open(os.devnull, 'w')
+    output = subprocess.check_output(command, stderr=dev_null)
 
     for line in output.splitlines():
         if b"bit_rate=" in line:
             bitrate = float(line.split(b"=")[1]) / 1024 / 1024
-            print("[bitrate] ", bitrate)
+            print(Back.WHITE + Fore.BLACK +  "[BITRATE]" + Style.RESET_ALL, end="")
+            print(f"\t{bitrate}")
             return bitrate
     return None
+
+
+def ffmpeg_cmd(video_file, output_file, bitrate, hwaccel=True):
+    if hwaccel:
+        return [
+            "ffmpeg", "-hide_banner",
+            "-v", "error",
+            "-stats", "-y",
+            "-hwaccel", "vaapi", "-vaapi_device", "/dev/dri/renderD128",
+            "-i", video_file,
+            "-vf", 'format=nv12,hwupload,scale_vaapi=w=-2:h=1080',
+            "-acodec", "copy", "-b:v", f"{bitrate}M",
+            "-vcodec", "h264_vaapi",
+            output_file
+        ]
+    else:
+        return [
+            "ffmpeg", "-hide_banner",
+            "-v", "error",
+            "-stats", "-y",
+            "-i", video_file,
+            "-vf",  "scale=-2:min(1080\,trunc(ih/2)*2)",
+            "-acodec", "copy", "-b:v", f"{bitrate}M",
+            "-vcodec", "libx264",
+            output_file
+        ]
 
 
 def transcode_video(video_file, output_file, bitrate):
     output_dir = os.path.dirname(output_file)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
+        
+    print(Back.WHITE + Fore.BLACK +  "[SOURCE] " + Style.RESET_ALL, end="")
+    print(f"\t{video_file}")
+    print(Back.WHITE + Fore.BLACK +  "[OUTPUT]" + Style.RESET_ALL, end="")
+    print(f"\t{output_file}")
 
-    command = [
-        "ffmpeg", "-hide_banner",
-        "-v", "fatal",
-        "-stats", "-y",
-        "-hwaccel", "vaapi", "-vaapi_device", "/dev/dri/renderD128",
-        "-i", video_file,
-        "-vf", 'format=nv12,hwupload,scale_vaapi=w=-2:h=1080',
-        "-acodec", "copy", "-b:v", f"{bitrate}M",
-        "-vcodec", "h264_vaapi",
-        output_file
-    ]
+    print(Back.GREEN + Fore.WHITE + '[FFMPEG]' + Style.RESET_ALL, end="")
+    print(f"\tProcessing file with VAAPI")
 
-    print(command)
+    command = ffmpeg_cmd(video_file, output_file, bitrate, True)
 
-    subprocess.run(command)
+    returncode = subprocess.run(command).returncode
+
+    if returncode != 0:
+        print(Back.BLUE + Fore.WHITE +
+              '[FFMPEG]' + Style.RESET_ALL, end="")
+        print(f"\tRetrying processing file with CPU")
+
+        retry_cpu = ffmpeg_cmd(video_file, output_file, bitrate, False)
+        retry = subprocess.run(retry_cpu).returncode
+
+        if retry != 0:
+            print(Back.RED + Fore.WHITE +
+                  '[FAIL]' + Style.RESET_ALL, end="")
+            print(f"\tFailed to process {video_file}")
+            return
 
     conn = connect_to_db()
-
     insert_transcoded_video(conn, video_file, output_file)
-
     conn.close()
 
 
 def transcode_videos_recursively(source_dir, output_dir, bitrate):
-    conn = connect_to_db()
-    create_table_if_not_exists(conn)
+    supported_ext = (".mp4", ".mkv", ".webm")
+    with connect_to_db() as conn:
+        create_table_if_not_exists(conn)
 
-    for root, dirs, files in os.walk(source_dir):
-        for file in files:
-            if file.endswith(".mp4") or file.endswith(".mkv"):
-                video_file = os.path.join(root, file)
-                output_file = os.path.join(
-                    output_dir, os.path.relpath(video_file, source_dir)
-                )
+        for root, dirs, files in os.walk(source_dir):
+            for file in files:
+                if file.endswith(supported_ext):
+                    video_file = os.path.join(root, file)
+                    output_file = os.path.join(
+                        output_dir, os.path.relpath(video_file, source_dir))
 
-                if check_if_transcoded(conn, video_file):
-                    continue
-                source_bitrate = get_bitrate(video_file)
+                    output_file = os.path.splitext(output_file)[0] + ".mp4"
+                    # continue
 
-                transcode_video(video_file, output_file,
-                                min(source_bitrate, bitrate))
+                    if check_if_transcoded(conn, video_file):
+                        continue
 
-    conn.close()
+                    if check_video(video_file):
+                        source_bitrate = 3 if file.endswith(
+                            ".webm") else get_bitrate(video_file)
+                        transcode_video(video_file, output_file, min(
+                            source_bitrate, bitrate))
 
 
 if __name__ == "__main__":
