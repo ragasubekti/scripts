672e002015ad6ba45e203921a8c2d8daf08ad4ae
Raga Subekti <ragasubekti@gmail.com>
2023-11-13 09:19:36 +0000

    add flatpak shim script

---
+++ flatpak_shimmy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+create_shims() {
+    _APPID="$1"
+    _COMMAND="$2"
+    shim="$HOME/.local/bin/$_COMMAND"
+
+    cat > $shim << EOF
+#!/usr/bin/env sh
+exec flatpak run $_APPID "\$@"
+EOF
+    chmod +x $shim
+}
+
+flatpak list --app --columns=application | while read _APPID; do
+    _CMD="$(flatpak info -m $appid | awk -F= '/^command=/ {print $2}')"
+    _CMD="$(basename $_CMD)"
+
+    create_shims "$_APPID" "$_CMD"
+done
